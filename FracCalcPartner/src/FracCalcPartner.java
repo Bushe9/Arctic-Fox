d767d891555cdac922313360fc573ff69dc24bfa
Baileys3 <42974372+Baileys3@users.noreply.github.com>
2018-11-20 09:11:39 -0800

    FracCalc

--- FracCalcPartner/src/FracCalcPartner.java
+++ FracCalcPartner/src/FracCalcPartner.java
@@ -3,110 +3,119 @@ import java.util.Scanner;
 public class FracCalcPartner {
 
 	public static void main(String[] args) {
+		// TODO: Read the input from the user and call produceAnswer with an equation
+		Scanner console = new Scanner(System.in);
+		System.out.print("Please enter an expression: ");
+		while (console.hasNextLine()) {
 
-		{
-			// TODO: Read the input from the user and call produceAnswer with an equation
-			System.out.println("Enter the next equation");
-			Scanner console = new Scanner(System.in);
-			//wholeNum();
-			//numerNum();
-			//produceAnswer();
-			produceAnswer(console.nextLine());
-			System.out.println(produceAnswer(console.nextLine()));
-			console.close(); }
+			String input = console.nextLine();
+			if (input.equals("quit")) {
+				break;
+			}
 		}
+	}
 
-		// ** IMPORTANT ** DO NOT DELETE THIS FUNCTION.  This function will be used to test your code
-		// This function takes a String 'input' and produces the result
-		//
-		// input is a fraction string that needs to be evaluated.  For your program, this will be the user input.
-		//      e.g. input ==> "1/2 + 3/4"
-		//        
-		// The function should return the result of the fraction after it has been calculated
-		//      e.g. return ==> "1_1/4"
-		public static String produceAnswer(String input) { 
-			// TODO: Implement this function to produce the solution to the input
-			Scanner passer = new Scanner(input);
-			//System.out.print(input);
-			String frac1 = passer.next();
-			String symbol = passer.next();
-			String frac2 = passer.next();
-			System.out.println(frac2);
-			int whole1 = wholeNum(frac1);
-			int whole2 = wholeNum(frac2);
-			int numerator1 = numerNum(frac1);
-			int numerator2 = numerNum(frac2);
+	// ** IMPORTANT ** DO NOT DELETE THIS FUNCTION.  This function will be used to test your code
+	// This function takes a String 'input' and produces the result
+	//
+	// input is a fraction string that needs to be evaluated.  For your program, this will be the user input.
+	//      e.g. input ==> "1/2 + 3/4"
+	//        
+	// The function should return the result of the fraction after it has been calculated
+	//      e.g. return ==> "1_1/4"
+	public static String produceAnswer(String input) { 
+		// TODO: Implement this function to produce the solution to the input
+		Scanner passer = new Scanner(input);
+		String frac1 = passer.next();
+		String symbol = passer.next();
+		String frac2 = passer.next();
+		int whole1 = wholeNum1(frac1);
+		int whole2 = wholeNum2(frac2);
+		int numerator1 = numerNum(frac1);
+		int numerator2 = numerNum(frac2);
+		int denominator1 = denomNum(frac1);
+		int denominator2 = denomNum(frac2);
+		if (symbol.contains("+")) {
+			int wholeAns = whole1 + whole2;
+			if (denominator1 == denominator2) {
+				int numeratorAns = numerator1 + numerator2;
+			} else {
+				numerator2 *= denominator1;
+				numerator1 *= denominator2;
+				denominator1 *= denominator2;
+				denominator2 = denominator1;
 
-			String denominator1 = denomNum(frac1);
-			String denominator2 = denomNum(frac2);
-			System.out.println(frac2);
-
-			passer.close();
-			return frac2;
+			}
 		}
-		public static String denomNum(String frac1) {
+		/*if(input.contains("+")) {
+			if(input.contains("_")) {
+				System.out.print(input.substring(0, input.indexOf('_')));
+				int whole1 = Integer.parseInt(input.substring(0, input.indexOf('_')));
+				System.out.print("_");
+				System.out.print(input.substring(input.indexOf('_') + 1, input.indexOf('/')));
+				int numerator1 = Integer.parseInt(input.substring(input.indexOf('_') + 1, input.indexOf('/')));
+				System.out.print("/");
+				System.out.println(input.substring(input.indexOf('/') + 1, input.length()));
+				int denominator1 = Integer.parseInt(input.substring(input.indexOf('/') + 1, input.length()));
+				int whole2 = Integer.parseInt(input.substring(input.indexOf('+') + 1, input.lastIndexOf('_')));
+				int numerator2 = Integer.parseInt(input.substring(input.lastIndexOf('_') + 1, input.lastIndexOf('/'))); 
+				int denominator2 = 
+				System.out.print("");
 
-			return null;
-		}
+	} else if(input.contains("/")){
+		System.out.print(input.substring(0,input.indexOf('/')));
+		int numerator = Integer.parseInt(input.substring(0,input.indexOf('/')));
+		System.out.print("/");
+		System.out.println(input.substring(input.indexOf('/') + 1, input.length()));
+		int denominator = Integer.parseInt(input.substring(input.indexOf('/') + 1, input.length()));
+	} else {
+		System.out.println(input.substring(0,input.length()));
+		int whole = Integer.parseInt(input.substring(0, input.indexOf('_')));
+	}*/
+		//passer.close();
+		return null;
+	}
 
-		public static int wholeNum(String frac) {
-			if (frac.contains("_")) {
-				return Integer.parseInt(frac.substring(0, frac.indexOf("_")));
+	public static int wholeNum1(String frac) {
+		if (frac.contains("_")) {
+			return Integer.parseInt(frac.substring(0, frac.indexOf("_")));
+		} else if (!frac.contains("/")) {
+			if (frac.contains("+")) {
+				return Integer.parseInt(frac.substring(0, frac.indexOf("+")));
 			} else {
 				return 0;
+
 			}
+
 		}
-		public static int numerNum(String frac) {
+		return 0;
+	}
+
+	public static int wholeNum2(String frac) {
+		if (frac.contains("_")) {
+			return Integer.parseInt(frac.substring(0, frac.indexOf("_")));
+		} else if (!frac.contains("/")) {
+			return Integer.parseInt(frac.substring(0, frac.indexOf("+")));
+		} else {
+			return 0;
+		}
+	}
+
+	public static int numerNum(String frac) {
+		if (frac.contains("/")) {
 			return Integer.parseInt(frac.substring(frac.indexOf('_') + 1, frac.indexOf('/')));
+		} else {
+			return 0;
 		}
+	}
 
-	
-	// TODO: Fill in the space below with any helper methods that you think you will need
-	//import java.util.Scanner;
-
-
-
-
-	/*public static void main(String[] args) {
-				fraction("1_4/5");
-				//fraction("27_2/3");
-				//fraction("2/3");
-				//fraction("1");
-				//equation("1_5/6 + 3_1/3");
-			}
-
-			public static String fraction(String x) {
-
-				if(x.contains("_")) {
-					System.out.print(x.substring(0, x.indexOf('_')));
-					Integer.parseInt(x.substring(0, x.indexOf('_')));
-					System.out.print("_");
-					System.out.print(x.substring(x.indexOf('_') + 1, x.indexOf('/')));
-					Integer.parseInt(x.substring(x.indexOf('_') + 1, x.indexOf('/')));
-					System.out.print("/");
-					System.out.println(x.substring(x.indexOf('/') + 1, x.length()));
-					Integer.parseInt(x.substring(x.indexOf('/') + 1, x.length()));
-
-					System.out.print("");
-
-				} else if(x.contains("/")){
-					System.out.print(x.substring(0,x.indexOf('/')));
-					Integer.parseInt(x.substring(0,x.indexOf('/')));
-					System.out.print("/");
-					System.out.println(x.substring(x.indexOf('/') + 1, x.length()));
-					Integer.parseInt(x.substring(x.indexOf('/') + 1, x.length()));
-				} else {
-					System.out.println(x.substring(0,x.length()));
-				}
-				return x;
-			}
-		}  
-			//int num = Integer.parseInt(x);*/
-
-
-
-
-
+	public static int denomNum(String frac) {
+		if (frac.contains("/")) {
+			return Integer.parseInt(frac.substring(frac.indexOf('/') + 1, frac.length())); 
+		} else {
+			return 1;
+		}
+	}
 }
 
 
